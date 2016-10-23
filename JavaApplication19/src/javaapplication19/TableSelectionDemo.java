30b055dd96ee6d467213651fe322bb3fa80e8a9a
Sérgio <Srgio@Lopes>
2016-10-23 15:50:43 -0200

    teste2

--- JavaApplication19/src/javaapplication19/TableSelectionDemo.java
+++ JavaApplication19/src/javaapplication19/TableSelectionDemo.java
@@ -30,7 +30,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
  
-package components;
+package javaapplication19;
  
 /*
  * TableSelectionDemo.java requires no other files.
